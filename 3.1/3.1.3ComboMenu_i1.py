e9e049fb47da85057c5b037e60b75bc6c776fab1
harsha201000 <138086385+harsha201000@users.noreply.github.com>
2025-05-16 16:09:51 -0500

    Add files via upload

---
+++ 3.1/3.1.3ComboMenu_i1.py
@@ -0,0 +1,3 @@
+# Iteration 1
+sandwich = input("What type of sandwich would you like? Chicken for $5.25, Beef for $6.25, Tofu for $5.75, or none? ")
+print("You ordered a " + sandwich + " sandwich.")
